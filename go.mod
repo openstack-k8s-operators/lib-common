0ee6c5221eaeb58670a01dfa0444994f22b5950f
Martin Schuppert <mschuppert@redhat.com>
2022-05-25 11:37:59 +0200

    add database functions + tests

--- go.mod
+++ go.mod
@@ -5,10 +5,30 @@ go 1.17
 require (
 	github.com/go-logr/logr v1.2.0
 	github.com/onsi/gomega v1.17.0
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	github.com/openstack-k8s-operators/keystone-operator v0.0.0-20220516151700-a6967a7f8a1b
+	github.com/openstack-k8s-operators/mariadb-operator v0.0.0-20220516121356-119f8d825a71
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e
 	k8s.io/api v0.23.6
 	k8s.io/apimachinery v0.23.6
 	sigs.k8s.io/controller-runtime v0.11.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.28.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
+	k8s.io/apiextensions-apiserver v0.23.0 // indirect
+	k8s.io/component-base v0.23.0 // indirect
 )
 
 require (
---
+++ pkg/database/funcs.go
@@ -0,0 +1,239 @@
+/*
+Copyright 2020 Red Hat
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/openstack-k8s-operators/lib-common/pkg/common"
+	"github.com/openstack-k8s-operators/lib-common/pkg/condition"
+	"github.com/openstack-k8s-operators/lib-common/pkg/helper"
+	mariadbv1 "github.com/openstack-k8s-operators/mariadb-operator/api/v1beta1"
+
+	corev1 "k8s.io/api/core/v1"
+	k8s_errors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// NewDatabase returns an initialized DB.
+func NewDatabase(
+	databaseName string,
+	databaseUser string,
+	secret string,
+	labels map[string]string,
+) *Database {
+	return &Database{
+		databaseName: databaseName,
+		databaseUser: databaseUser,
+		secret:       secret,
+		labels:       labels,
+	}
+}
+
+//
+// getDatabaseServiceName - get the service name of the DB
+//
+func getDatabaseServiceName(
+	ctx context.Context,
+	h *helper.Helper,
+) (string, condition.Condition, error) {
+
+	var databaseHostname string
+	selector := map[string]string{
+		"app": "mariadb",
+	}
+	serviceList, err := common.GetServicesListWithLabel(
+		ctx,
+		h,
+		h.GetBeforeObject().GetNamespace(),
+		selector,
+	)
+	if err != nil {
+		msg := fmt.Sprintf("Error getting the DB service using label %v", selector)
+		cond := condition.NewCondition(
+			condition.TypeError,
+			corev1.ConditionTrue,
+			ReasonDBServiceNameError,
+			msg)
+
+		return databaseHostname, cond, err
+	}
+
+	// can we expect there is only one DB instance per namespace?
+	if len(serviceList.Items) > 1 {
+		msg := fmt.Sprintf("more then one DB service found %d", len(serviceList.Items))
+		cond := condition.NewCondition(
+			condition.TypeError,
+			corev1.ConditionTrue,
+			ReasonDBServiceNameError,
+			msg)
+
+		return databaseHostname, cond, err
+	}
+	databaseHostname = serviceList.Items[0].GetName()
+
+	return databaseHostname, condition.Condition{}, nil
+}
+
+//
+// GetDatabaseHostname - returns the DB hostname which host the DB
+//
+func (d *Database) GetDatabaseHostname() string {
+	return d.databaseHostname
+}
+
+//
+// CreateOrPatchDB - create or patch the service DB instance
+//
+func (d *Database) CreateOrPatchDB(
+	ctx context.Context,
+	h *helper.Helper,
+) (condition.Condition, ctrl.Result, error) {
+
+	db := &mariadbv1.MariaDBDatabase{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      h.GetBeforeObject().GetName(),
+			Namespace: h.GetBeforeObject().GetNamespace(),
+		},
+		Spec: mariadbv1.MariaDBDatabaseSpec{
+			// the DB name must not change, therefore specify it outside the mutuate function
+			Name: d.databaseName,
+		},
+	}
+
+	// set the database hostname on the db instance
+	databaseServiceName, cond, err := getDatabaseServiceName(ctx, h)
+	if err != nil {
+		return cond, ctrl.Result{}, err
+	}
+	d.databaseHostname = databaseServiceName
+
+	op, err := controllerutil.CreateOrPatch(ctx, h.GetClient(), db, func() error {
+		// TODO Labels
+		db.Labels = common.MergeStringMaps(
+			db.GetLabels(),
+			d.labels,
+		)
+
+		db.Spec.Secret = d.secret
+
+		err := controllerutil.SetControllerReference(h.GetBeforeObject(), db, h.GetScheme())
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil && !k8s_errors.IsNotFound(err) {
+		msg := fmt.Sprintf("Error create or update DB object %s", db.Name)
+		c := condition.NewCondition(
+			condition.TypeError,
+			corev1.ConditionTrue,
+			ReasonDBPatchError,
+			msg)
+
+		return c, ctrl.Result{}, fmt.Errorf("%s - %s", msg, err.Error())
+	}
+
+	if op != controllerutil.OperationResultNone {
+		msg := fmt.Sprintf("DB object %s created or patched", db.Name)
+		c := condition.NewCondition(
+			condition.TypeCreated,
+			corev1.ConditionTrue,
+			ReasonDBPatchOK,
+			msg,
+		)
+		return c, ctrl.Result{RequeueAfter: time.Second * 5}, nil
+	}
+
+	return condition.Condition{}, ctrl.Result{}, nil
+}
+
+//
+// WaitForDBCreated - wait until the MariaDBDatabase is initialized and reports Status.Completed == true
+//
+func (d *Database) WaitForDBCreated(
+	ctx context.Context,
+	h *helper.Helper,
+) (condition.Condition, ctrl.Result, error) {
+
+	db, cond, err := d.GetDBWithName(
+		ctx,
+		h,
+	)
+	if err != nil && !k8s_errors.IsNotFound(err) {
+		return cond, ctrl.Result{}, err
+	}
+
+	if !db.Status.Completed || k8s_errors.IsNotFound(err) {
+		msg := fmt.Sprintf("Waiting for service DB %s to be created", db.Name)
+		cond := condition.NewCondition(
+			condition.TypeWaiting,
+			corev1.ConditionTrue,
+			ReasonDBWaitingInitialized,
+			msg)
+		return cond, ctrl.Result{RequeueAfter: time.Second * 5}, nil
+	}
+
+	return condition.Condition{}, ctrl.Result{}, nil
+}
+
+//
+// GetDBWithName - get DB object with name in namespace
+//
+func (d *Database) GetDBWithName(
+	ctx context.Context,
+	h *helper.Helper,
+) (*mariadbv1.MariaDBDatabase, condition.Condition, error) {
+	db := &mariadbv1.MariaDBDatabase{}
+	err := h.GetClient().Get(
+		ctx,
+		types.NamespacedName{
+			Name:      d.databaseName,
+			Namespace: h.GetBeforeObject().GetNamespace(),
+		},
+		db)
+	if err != nil {
+		if k8s_errors.IsNotFound(err) {
+			msg := fmt.Sprintf("Failed to get %s database %s ", d.databaseName, h.GetBeforeObject().GetNamespace())
+			cond := condition.NewCondition(
+				condition.TypeError,
+				corev1.ConditionTrue,
+				ReasonDBNotFound,
+				msg)
+
+			return db, cond, common.WrapErrorForObject(msg, h.GetBeforeObject(), err)
+		}
+
+		msg := fmt.Sprintf("DB error %s %s ", d.databaseName, h.GetBeforeObject().GetNamespace())
+		cond := condition.NewCondition(
+			condition.TypeError,
+			corev1.ConditionTrue,
+			ReasonDBError,
+			msg)
+
+		return db, cond, common.WrapErrorForObject(msg, h.GetBeforeObject(), err)
+	}
+
+	return db, condition.Condition{}, nil
+}
