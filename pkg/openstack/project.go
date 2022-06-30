9d45e8013c8f5dfbb400ef021f510ec4794c4719
Martin Schuppert <mschuppert@redhat.com>
2022-06-30 14:03:47 +0200

    Add openstack module
    Adds openstack module with functions to manage
    - client
    - endpoints
    - project
    - role
    - service
    - user
    
    E.g. used in KeystoneService to register users for service
    components.
    
    Usage is to use NewOpenStack() to get a new instance of OpenStack
    with an authenticated client. Then can do o.CreateEndpoint(...)
    to create a new service endpoint.
    
    Implemented as a new module to not have dependency to gopher
    on remaining lib-common content, where these are not used.

---
+++ pkg/openstack/project.go
@@ -0,0 +1,66 @@
+/*
+Copyright 2022 Red Hat
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package openstack
+
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+	projects "github.com/gophercloud/gophercloud/openstack/identity/v3/projects"
+)
+
+// Project -
+type Project struct {
+	Name        string
+	Description string
+}
+
+//
+// CreateProject - creates project with projectName and projectDescription if it does not exist
+//
+func (o *OpenStack) CreateProject(
+	log logr.Logger,
+	p Project,
+) (string, error) {
+	var projectID string
+	allPages, err := projects.List(o.osclient, projects.ListOpts{Name: p.Name}).AllPages()
+	if err != nil {
+		return projectID, err
+	}
+	allProjects, err := projects.ExtractProjects(allPages)
+	if err != nil {
+		return projectID, err
+	}
+	if len(allProjects) == 1 {
+		projectID = allProjects[0].ID
+	} else if len(allProjects) == 0 {
+		createOpts := projects.CreateOpts{
+			Name:        p.Name,
+			Description: p.Description,
+		}
+		log.Info(fmt.Sprintf("Creating project %s", p.Name))
+		project, err := projects.Create(o.osclient, createOpts).Extract()
+		if err != nil {
+			return projectID, err
+		}
+		projectID = project.ID
+	} else {
+		return projectID, fmt.Errorf(fmt.Sprintf("multiple projects named \"%s\" found", p.Name))
+	}
+
+	return projectID, nil
+}
