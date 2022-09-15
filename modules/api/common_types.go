42f1a8107c75c45a7a69b22400bc57ddf285195c
Balazs Gibizer <gibi@redhat.com>
2022-09-15 12:03:35 +0200

    Move OpenStackServiceBase API struct to lib-common/api module
    Each OpenStack service CRD will has a set of common fields like
    ContainerImage, Replicas, NodeSelector, etc. So factor these common
    fields out to a struct and move it to a common place. It is suggested
    that each operator import this struct and embed it to the Spec struct
    via:
    
    	api.OpenStackServiceBase `json:",inline"`
    
    The embedding and the inline tags makes this change almost fully backward
    compatible with the original direct fields. The CRD will not change as
    it will have the fields from OpenStackServiceBase directly inlined.
    The controller code that reads Spec fields does not have to change as
    the fields are directly accessible in the Spec struct due to unambiguous
    embedding. However in the code where the Spec is instantiated (e.g. in
    the tests) the Spec instantiation needs to use the
    
        OpenStackServiceBase: api.OpenStackServiceBase{...}
    
    syntax to populate the embedded struct fields.

---
+++ modules/api/common_types.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2022 Red Hat
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package api
+
+import (
+	corev1 "k8s.io/api/core/v1"
+)
+
+// +kubebuilder:object:generate:=true
+
+// OpenStackServiceBase contains the fields that are needed for each OpenStack service CRD
+type OpenStackServiceBase struct {
+	// +kubebuilder:validation:Required
+	// The service specific Container Image URL
+	ContainerImage string `json:"containerImage"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Maximum=32
+	// +kubebuilder:validation:Minimum=0
+	// Replicas of the service to run
+	Replicas int32 `json:"replicas"`
+
+	// +kubebuilder:validation:Optional
+	// NodeSelector to target subset of worker nodes running this service
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:default="# add your customization here"
+	// CustomServiceConfig - customize the service config using this parameter to change service defaults,
+	// or overwrite rendered information using raw OpenStack config format. The content gets added to
+	// to /etc/<service>/<service>.conf.d directory as custom.conf file.
+	CustomServiceConfig string `json:"customServiceConfig,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// ConfigOverwrite - interface to overwrite default config files like e.g. logging.conf or policy.json.
+	// But can also be used to add additional files. Those get added to the service config dir in /etc/<service> .
+	DefaultConfigOverwrite map[string]string `json:"defaultConfigOverwrite,omitempty"`
+
+	// +kubebuilder:validation:Optional
+	// Resources - Compute Resources required by this service (Limits/Requests).
+	// https://kubernetes.io/docs/concepts/configuration/manage-resources-containers/
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
+}
