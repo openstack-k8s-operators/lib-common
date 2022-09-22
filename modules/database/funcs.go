0d8e75a4305586d77ece180c2997a309b33a164e
Balazs Gibizer <gibi@redhat.com>
2022-09-22 19:32:33 +0200

    Support multiple DB service instances
    To support multi cell deployment we need to support
    multiple DB service instances as in a realistic multi cell deployment
    each Nova cell needs its own DB service instance.
    
    So this patch introduces CreateOrPatchByName to the database module
    where the caller can specify the name of the MariaDB CR to filter for a
    specific k8s Service instance to use for its DB schema. The existing
    and heavily used CreateOrPatchDB is defaulted to use the "default" DB
    service which is named "openstack" today but deprecated for removal
    after each service controller starts using  CreateOrPatchDBByName
    instead.

--- modules/database/funcs.go
+++ modules/database/funcs.go
@@ -50,14 +50,22 @@ func NewDatabase(
 
 //
 // setDatabaseHostname - set the service name of the DB as the databaseHostname
+// by looking up the Service via the name of the MariaDB CR which provides it.
 //
 func (d *Database) setDatabaseHostname(
 	ctx context.Context,
 	h *helper.Helper,
+	name string,
 ) error {
 
+	// When the MariaDB CR provides the Service it sets the "cr" label of the
+	// Service to "mariadb-<name of the MariaDB CR>". So we use this label
+	// to select the right Service. See:
+	// https://github.com/openstack-k8s-operators/mariadb-operator/blob/5781b0cf1087d7d28fa285bd5c44689acba92183/pkg/service.go#L17
+	// https://github.com/openstack-k8s-operators/mariadb-operator/blob/590ffdc5ad86fe653f9cd8a7102bb76dfe2e36d1/pkg/utils.go#L4
 	selector := map[string]string{
 		"app": "mariadb",
+		"cr":  fmt.Sprintf("mariadb-%s", name),
 	}
 	serviceList, err := service.GetServicesListWithLabel(
 		ctx,
@@ -70,7 +78,9 @@ func (d *Database) setDatabaseHostname(
 			selector, err)
 	}
 
-	// can we expect there is only one DB instance per namespace?
+	// We assume here that a MariaDB CR instance always creates a single
+	// Service. If multiple DB services are used the they are managed via
+	// separate MariaDB CRs.
 	if len(serviceList.Items) > 1 {
 		return util.WrapErrorForObject(
 			fmt.Sprintf("more then one DB service found %d", len(serviceList.Items)),
@@ -99,10 +109,26 @@ func (d *Database) GetDatabase() *mariadbv1.MariaDBDatabase {
 
 //
 // CreateOrPatchDB - create or patch the service DB instance
+// Deprecated. Use CreateOrPatchDBByName instead. If you want to use the
+// default the DB service instance of the deployment then pass "openstack" as
+// the name.
 //
 func (d *Database) CreateOrPatchDB(
 	ctx context.Context,
 	h *helper.Helper,
+) (ctrl.Result, error) {
+	return d.CreateOrPatchDBByName(ctx, h, "openstack")
+}
+
+//
+// CreateOrPatchDBByName - create or patch the service DB instance on
+// the DB service. The DB service is selected by the name of the MariaDB CR
+// providing the service.
+//
+func (d *Database) CreateOrPatchDBByName(
+	ctx context.Context,
+	h *helper.Helper,
+	name string,
 ) (ctrl.Result, error) {
 
 	db := &mariadbv1.MariaDBDatabase{
@@ -117,7 +143,7 @@ func (d *Database) CreateOrPatchDB(
 	}
 
 	// set the database hostname on the db instance
-	err := d.setDatabaseHostname(ctx, h)
+	err := d.setDatabaseHostname(ctx, h, name)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
