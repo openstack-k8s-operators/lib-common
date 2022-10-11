58fcdfd771c97b0ee7417c4d6c0b2f341ffaaafb
Martin Schuppert <mschuppert@redhat.com>
2022-10-11 17:23:25 +0200

    add TestDistributePods() unit test

---
+++ modules/common/affinity/affinity_test.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2020 Red Hat
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package affinity
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var affinityObj = &corev1.Affinity{
+	PodAntiAffinity: &corev1.PodAntiAffinity{
+		PreferredDuringSchedulingIgnoredDuringExecution: []corev1.WeightedPodAffinityTerm{
+			{
+				PodAffinityTerm: corev1.PodAffinityTerm{
+					LabelSelector: &metav1.LabelSelector{
+						MatchExpressions: []metav1.LabelSelectorRequirement{
+							{
+								Key:      "ThisSelector",
+								Operator: metav1.LabelSelectorOpIn,
+								Values:   []string{"selectorValue1", "selectorValue2"},
+							},
+						},
+					},
+					TopologyKey: "ThisTopologyKey",
+				},
+				Weight: 1,
+			},
+		},
+	},
+}
+
+func TestDistributePods(t *testing.T) {
+
+	t.Run("Default pod distribution", func(t *testing.T) {
+		g := NewWithT(t)
+
+		d := DistributePods("ThisSelector", []string{"selectorValue1", "selectorValue2"}, "ThisTopologyKey")
+
+		g.Expect(d).To(BeEquivalentTo(affinityObj))
+	})
+}
