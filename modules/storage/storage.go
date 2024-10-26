333dbab71ed99b4200920d25d060096338d43168
Dan Prince <dprince@redhat.com>
2024-10-26 07:49:56 -0400

    Add Projected to custom VolumeSource struct

--- modules/storage/storage.go
+++ modules/storage/storage.go
@@ -90,6 +90,9 @@ type VolumeSource struct {
 	// csi (Container Storage Interface) represents ephemeral storage that is handled by certain external CSI drivers (Beta feature).
 	// +optional
 	CSI *corev1.CSIVolumeSource `json:"csi,omitempty" protobuf:"bytes,28,opt,name=csi"`
+
+	// projected items for all in one resources secrets, configmaps, and downward API
+	Projected *corev1.ProjectedVolumeSource `json:"projected,omitempty" protobuf:"bytes,26,opt,name=projected"`
 }
 
 // Volume our slimmed down version of Volume
@@ -162,7 +165,7 @@ func (s *VolumeSource) ToCoreVolumeSource() (*corev1.VolumeSource, error) {
 	return coreVolumeSource, nil
 }
 
-// ToCoreVolume- convert Volume to corev1.Volume
+// ToCoreVolume - convert Volume to corev1.Volume
 func (s *Volume) ToCoreVolume() (*corev1.Volume, error) {
 	coreVolume := &corev1.Volume{}
 
