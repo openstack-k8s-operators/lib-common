0967087f9106b59473ccbde925137719f841fa72
Francesco Pantano <fpantano@redhat.com>
2024-11-18 15:48:47 +0100

    Expose PodAffinity and PodAntiAffinity struct and build overrides
    This patch introduces a very basic struct to expose Pod Affinity/Antiaffinity
    and NodeAffinity interfaces as part of the Topology CR spec and it allows to
    patch the (opinionated) default that is currently applied to the services
    through DistributePods function call.
    The existing DistributePods function is still present in the code to ensure
    backward compatibility and not block lib-common bumps, but is marked as
    deprecated.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- modules/common/affinity/affinity.go
+++ modules/common/affinity/affinity.go
@@ -17,12 +17,20 @@ limitations under the License.
 package affinity
 
 import (
+	"encoding/json"
+	"fmt"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
 // DistributePods - returns rule to ensure that two replicas of the same selector
 // should not run if possible on the same worker node
+// Deprecated: This function is still maintained to ensure backward compatibility
+// with operators that need to bump lib-common without requiring to update the
+// signature of the DistributePods function, which is present in all StatefulSets/
+// Deployment definitions.
 func DistributePods(
 	selectorKey string,
 	selectorValues []string,
@@ -53,4 +61,79 @@ func DistributePods(
 			},
 		},
 	}
+}
+
+// DistributePodsWithOverrides - returns rule to ensure that two replicas of
+// the same selector should not run if possible on the same worker node
+func DistributePodsWithOverrides(
+	selectorKey string,
+	selectorValues []string,
+	topologyKey string,
+	overrides *OverrideSpec,
+) (*corev1.Affinity, error) {
+	defaultAffinity := &corev1.Affinity{
+		PodAntiAffinity: &corev1.PodAntiAffinity{
+			// This rule ensures that two replicas of the same selector
+			// should not run if possible on the same worker node
+			PreferredDuringSchedulingIgnoredDuringExecution: []corev1.WeightedPodAffinityTerm{
+				{
+					PodAffinityTerm: corev1.PodAffinityTerm{
+						LabelSelector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      selectorKey,
+									Operator: metav1.LabelSelectorOpIn,
+									Values:   selectorValues,
+								},
+							},
+						},
+						// usually corev1.LabelHostname "kubernetes.io/hostname"
+						// https://github.com/kubernetes/api/blob/master/core/v1/well_known_labels.go#L20
+						TopologyKey: topologyKey,
+					},
+					Weight: 100,
+				},
+			},
+		},
+	}
+	// patch the default affinity Object with the data passed as input
+	if overrides != nil {
+		patchedAffinity, err := toCoreAffinity(defaultAffinity, overrides)
+		return patchedAffinity, err
+	}
+	return defaultAffinity, nil
+}
+
+func toCoreAffinity(
+	affinity *v1.Affinity,
+	override *OverrideSpec,
+) (*v1.Affinity, error) {
+
+	aff := &v1.Affinity{
+		PodAntiAffinity: affinity.PodAntiAffinity,
+		PodAffinity:     affinity.PodAffinity,
+	}
+	if override != nil {
+		origAffinit, err := json.Marshal(affinity)
+		if err != nil {
+			return aff, fmt.Errorf("error marshalling Affinity Spec: %w", err)
+		}
+		patch, err := json.Marshal(override)
+		if err != nil {
+			return aff, fmt.Errorf("error marshalling Affinity Spec: %w", err)
+		}
+
+		patchedJSON, err := strategicpatch.StrategicMergePatch(origAffinit, patch, v1.Affinity{})
+		if err != nil {
+			return aff, fmt.Errorf("error patching Affinity Spec: %w", err)
+		}
+
+		patchedSpec := v1.Affinity{}
+		err = json.Unmarshal(patchedJSON, &patchedSpec)
+		if err != nil {
+			return aff, fmt.Errorf("error unmarshalling patched Service Spec: %w", err)
+		}
+		aff = &patchedSpec
+	}
+	return aff, nil
 }
--- modules/common/affinity/affinity_test.go
+++ modules/common/affinity/affinity_test.go
@@ -47,6 +47,30 @@ var affinityObj = &corev1.Affinity{
 	},
 }
 
+// weightedPodAffinityTermOverride represents an Override passed to the Affinity
+// tests
+var weightedPodAffinityTermOverride = []corev1.WeightedPodAffinityTerm{
+	{
+		PodAffinityTerm: corev1.PodAffinityTerm{
+			LabelSelector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{
+						Key:      "CustomKeySelector",
+						Operator: metav1.LabelSelectorOpIn,
+						Values: []string{
+							"selectorValue1",
+							"selectorValue2",
+							"selectorValue3",
+						},
+					},
+				},
+			},
+			TopologyKey: "CustomTopologyKey",
+		},
+		Weight: 80,
+	},
+}
+
 func TestDistributePods(t *testing.T) {
 
 	t.Run("Default pod distribution", func(t *testing.T) {
@@ -55,5 +79,61 @@ func TestDistributePods(t *testing.T) {
 		d := DistributePods("ThisSelector", []string{"selectorValue1", "selectorValue2"}, "ThisTopologyKey")
 
 		g.Expect(d).To(BeEquivalentTo(affinityObj))
+	})
+}
+
+func TestDistributePodsOverride(t *testing.T) {
+
+	t.Run("Default pod distribution", func(t *testing.T) {
+		g := NewWithT(t)
+		d, _ := DistributePodsWithOverrides("ThisSelector", []string{"selectorValue1", "selectorValue2"}, "ThisTopologyKey", nil)
+		g.Expect(d).To(BeEquivalentTo(affinityObj))
+	})
+
+	// Override the default AntiAffinity
+	t.Run("Pod distribution with overrides", func(t *testing.T) {
+		// The resulting affinity that should be assigned to the Pod
+		var expectedAffinity = &corev1.Affinity{
+			PodAffinity:  nil,
+			NodeAffinity: nil,
+			PodAntiAffinity: &corev1.PodAntiAffinity{
+				PreferredDuringSchedulingIgnoredDuringExecution: weightedPodAffinityTermOverride,
+			},
+		}
+		affinityOverride := &OverrideSpec{
+			PodAffinity: nil,
+			PodAntiAffinity: &corev1.PodAntiAffinity{
+				PreferredDuringSchedulingIgnoredDuringExecution: weightedPodAffinityTermOverride,
+			},
+			NodeAffinity: nil,
+		}
+		g := NewWithT(t)
+		d, _ := DistributePodsWithOverrides("ThisSelector", []string{"selectorValue1", "selectorValue2"}, "ThisTopologyKey", affinityOverride)
+		g.Expect(d).To(BeEquivalentTo(expectedAffinity))
+	})
+
+	// Override the Affinity but keep the default AntiAffinity
+	t.Run("Pod distribution with overrides", func(t *testing.T) {
+		// The resulting affinity that should be assigned to the Pod
+		var expectedAffinity = &corev1.Affinity{
+			// the default PodAntiAffinity defined in the DistributePods function
+			// is applied, while PodAffinity is the result of the override passed
+			// as input
+			PodAntiAffinity: affinityObj.PodAntiAffinity,
+			NodeAffinity:    nil,
+			PodAffinity: &corev1.PodAffinity{
+				PreferredDuringSchedulingIgnoredDuringExecution: weightedPodAffinityTermOverride,
+			},
+		}
+		affinityOverride := &OverrideSpec{
+			PodAntiAffinity: nil,
+			PodAffinity: &corev1.PodAffinity{
+				PreferredDuringSchedulingIgnoredDuringExecution: weightedPodAffinityTermOverride,
+			},
+			NodeAffinity: nil,
+		}
+		g := NewWithT(t)
+		d, _ := DistributePodsWithOverrides("ThisSelector", []string{"selectorValue1", "selectorValue2"}, "ThisTopologyKey", affinityOverride)
+		g.Expect(d).To(BeEquivalentTo(expectedAffinity))
 	})
 }
